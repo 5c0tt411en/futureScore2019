a6fec937fe953d6bb742e0173bd8aea563af35f4
scott <5c0tt.411en@gmail.com>
2019-02-20 09:11:28 +0900

    updated

--- futureScore2019/src/ofApp.cpp
+++ futureScore2019/src/ofApp.cpp
@@ -4,25 +4,30 @@ using namespace ofxCv;
 using namespace cv;
 
 void ofApp::setup() {
-    ofSetWindowShape(1080, 1920);
+#ifdef MAC
+    ofSetWindowPosition(-1080, 0);
+    ofSetFullscreen(true);
+#endif
     ofEnableBlendMode(OF_BLENDMODE_ALPHA);
     ofSetVerticalSync(true);
     ofSetFrameRate(60);
     finder.setup("haarcascade_frontalface_default.xml");
     finder.setPreset(ObjectFinder::Fast);
+    finder.setMinSizeScale(0.001);
     cam.setup(camSize.x, camSize.y);
-    
-//    dir.allowExt("png");
-//    for (int i = 0; i < dir.listDir("types/"); i++) {
-//        types.push_back(ofImage(dir.getPath(i)));
-//    }
-//    for (int i = 0; i < 5; i++)
-//        types.push_back(ofImage("types/" + ofToString(i) + ".png"));
     img.load("types/" + ofToString(0) + ".png");
+    
+    font.load("Roboto-Regular.ttf", 50);
 }
 
 void ofApp::update() {
+    ofHideCursor();
     time = ofGetElapsedTimef() - timeStamp;
+    valTime = ofGetElapsedTimef() - valTimeStamp;
+//    if (valTime >= 60) {
+//        valTimeStamp = ofGetElapsedTimef();
+//        val = ofRandom(8000, 12000);
+//    }
     cam.update();
     if(cam.isFrameNew()) {
         finder.update(cam);
@@ -30,26 +35,113 @@ void ofApp::update() {
 }
 
 void ofApp::draw() {
-    ofSetColor(255, 255);
-    cam.draw(853+1080, 0, -3413, 1920);
+    ofSetColor(255);
+    cam.draw(
+            (camDisplaySize.x - ofGetWidth()) / 2 + ofGetWidth(),
+            0,
+            -camDisplaySize.x,
+            camDisplaySize.y
+            );
+    if (finder.size() == 1) {
+        progressTime = ofGetElapsedTimef() - progressTimeStamp + progressTimeStamp2;
+        resetTimeStamp = ofGetElapsedTimef();
+        rec = true;
+//        if (progressTime >= judgeThre) {
+//            isProcessDone = true;
+//        }
+    }
+    else if (finder.size() == 0) {
+        if (rec) {
+            progressTimeStamp2 = progressTime;
+            rec = false;
+        }
+        resetTime = ofGetElapsedTimef() - resetTimeStamp;
+        progressTimeStamp = ofGetElapsedTimef();
+        if (resetTime >= resetThre) {
+            isProcessDone = false;
+            progressTimeStamp2 = 0;
+        }
+    }
+    for ( int i=0; i < finder.size(); i++){
+        if (finder.size() == 1) {
+            ofRectangle rect = finder.getObjectSmoothed(i);
+            ofVec4f converted = ofVec4f(
+                                        (camDisplaySize.x - ofGetWidth()) / 2 + ofGetWidth() - rect.getMinX() * camDisplaySize.x / camSize.x,
+                                        rect.getMinY() * camDisplaySize.x / camSize.x,
+                                        - rect.getWidth() * camDisplaySize.x / camSize.x,
+                                        rect.getHeight() * camDisplaySize.x / camSize.x
+                                        );
+            ofSetColor(255, 0, 0);
+            ofNoFill();
+            ofDrawRectangle(converted.x, converted.y, converted.z, converted.w);
+            ofSetColor(255);
+            ofFill();
+            if (isProcessDone) {
+                //            ofDrawRectangle(converted.x, converted.y - 5, converted.z, -30);
+                switch (roomNum) {
+                    case 1:
+                        font.drawString("Physical condition: " + judgeSt, converted.x + converted.z, converted.y - 10);
+                        break;
+                    case 2:
+                        font.drawString("Behavior: " + judgeSt, converted.x + converted.z, converted.y - 10);
+                        break;
+                    case 3:
+                        font.drawString(ofToString(int(val + ofNoise(ofGetElapsedTimef() / 30) * 100)), converted.x + converted.z, converted.y - 10);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else {
+                font.drawString("Under calculation...", converted.x + converted.z, converted.y - 40);
+                ofDrawRectangle(converted.x + converted.z, converted.y - 5, - converted.z * (progressTime) / judgeThre, -30);
+                if (progressTime >= judgeThre) {
+                    isProcessDone = true;
+                    judgeRandom = int(ofRandom(1, 4));
+                    switch (judgeRandom) {
+                        case 1:
+                            judgeSt = "PERFECT";
+                            break;
+                        case 2:
+                            judgeSt = "GREAT";
+                            break;
+                        case 3:
+                            judgeSt = "GOOD";
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+        
+    }
+    
     ofPushMatrix();
-    ofTranslate(853+1080, 0);
-    ofScale(-3413 / 640, 3413 / 640);
-    finder.draw();
+    {
+        ofTranslate((camDisplaySize.x - ofGetWidth()) / 2 + ofGetWidth(), 0);
+        ofScale(-camDisplaySize.x / camSize.x, camDisplaySize.x / camSize.x);
+        ofSetLineWidth(lineW);
+        ofSetColor(255, 0, 0);
+    }
     ofPopMatrix();
 
+#ifdef DEBUG
+    ofSetColor(255);
     ofPushMatrix();
-    ofScale(5, 5);
-    ofDrawBitmapStringHighlight(ofToString(finder.size()), 10, 20);
-    ofDrawBitmapStringHighlight("fps: " + ofToString(ofGetFrameRate()), 10, 50);
+    {
+        ofScale(5, 5);
+        ofDrawBitmapStringHighlight(ofToString(finder.size()), 10, 20);
+        ofDrawBitmapStringHighlight("fps: " + ofToString(ofGetFrameRate()), 10, 50);
+    }
     ofPopMatrix();
+#endif
     
     switch (mode) {
         case 0: //FADE IN
                 ofSetColor(255, time * 255);
-//                types[n].draw(0, 0);
                 img.draw(0,0);
-            if (time >= 1) {
+            if (time >= fadeInTime) {
                 timeStamp = ofGetElapsedTimef();
                 time = ofGetElapsedTimef() - timeStamp;
                 mode = 1;
@@ -57,37 +149,24 @@ void ofApp::draw() {
             break;
         case 1: // STAY
                 ofSetColor(255);
-//                types[n].draw(0, 0);
                 img.draw(0,0);
-            if (time >= 5) {
+            if (time >= stayTime) {
                 timeStamp = ofGetElapsedTimef();
                 time = ofGetElapsedTimef() - timeStamp;
                 mode = 2;
             }
             break;
         case 2: //FADE OUT
-                ofSetColor(255, (1 - time) * 255);
-//                types[n].draw(0, 0);
+                ofSetColor(255, (fadeOutTime - time) * 255);
                 img.draw(0,0);
-            if (time >= 1) {
+            if (time >= fadeOutTime) {
                 timeStamp = ofGetElapsedTimef();
                 time = ofGetElapsedTimef() - timeStamp;
                 mode = 3;
-//                if (n == dir.listDir("types/") - 1) {
-//                    n = 0;
-//                }
-//                else {
-//                    n++;
-//                }
-//                ofLog() << n << endl;
-//                img.load("types/" + ofToString(n) + ".png");
             }
             break;
         case 3: //WAIT
-//            ofSetColor(255);
-            //                types[n].draw(0, 0);
-//            img.draw(0,0);
-            if (time >= 1) {
+            if (time >= waitTime) {
                 timeStamp = ofGetElapsedTimef();
                 time = ofGetElapsedTimef() - timeStamp;
                 mode = 0;
--- futureScore2019/src/ofApp.h
+++ futureScore2019/src/ofApp.h
@@ -3,6 +3,9 @@
 #include "ofMain.h"
 #include "ofxCv.h"
 
+//#define DEBUG
+#define MAC
+
 class ofApp : public ofBaseApp {
 public:
 	void setup();
@@ -12,10 +15,38 @@ public:
 	ofVideoGrabber cam;
 	ofxCv::ObjectFinder finder;
 
-    ofVec2f camSize = ofVec2f(1280, 720);
-//    vector <ofImage> types;
+    ofVec2f
+        camSize = ofVec2f(1280, 720),
+        camDisplaySize = ofVec2f(1920 * camSize.x / camSize.y, 1920);
+    
     ofImage img;
-    ofDirectory dir;
-    float time = 0, timeStamp = 0;
-    int mode = 0, n = 0;
+    
+    float
+        time = 0,
+        timeStamp = 0,
+        valTime = 0,
+        valTimeStamp = 0,
+        progressTime = 0,
+        progressTimeStamp = 0,
+        progressTimeStamp2 = 0,
+        resetTime = 0,
+        resetTimeStamp = 0,
+        fadeInTime = 1.0,
+        stayTime = 10.0,
+        fadeOutTime = 1.0,
+        waitTime = 1.0,
+        judgeThre = 5,
+        resetThre = 10;
+    int
+        mode = 0,
+        lineW = 4,
+        fontSize = 50,
+        val = ofRandom(8000, 12000),
+        roomNum = 1,
+        judgeRandom;
+    bool
+        isProcessDone = false,
+        rec = false;
+    string judgeSt = "";
+    ofTrueTypeFont font;
 };
